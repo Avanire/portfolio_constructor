5b79867d71aaafdf1ea1366335a6fc1e57a9f373
Avanire <drtony@yandex.ru>
2022-12-25 09:36:42 +0600

    fix description

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import styles from './Contacts.module.css';
 import PropTypes from "prop-types";
 
-const Contacts = ({heading, value}) => {
+const Contacts = ({heading, value, link}) => {
     return (
         <section className={styles.contact}>
             <div className={styles.heading}>{heading}</div>
-            <div>{value}</div>
+            {link ? <a href={link} className={styles.link}>{value}</a> : <div>{value}</div>}
         </section>
     );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,6 @@
 import React from "react";
 import styles from './Header.module.css';
-import {
-    AVATAR,
-    NAME,
-    SURNAME,
-    ROLE,
-    SOCIAL_LINK_GIT,
-    SOCIAL_LINK_MAIL,
-    CONTACTS_HEADER
-} from '../../utils/constant';
+import {AVATAR, CONTACTS_HEADER, NAME, ROLE, SOCIAL_LINK_GIT, SOCIAL_LINK_MAIL, SURNAME} from '../../utils/constant';
 import Social from "../Social/Social";
 import gitImage from '../../images/github.svg';
 import mailImage from '../../images/envelope-regular.svg';
@@ -19,19 +11,20 @@ const Header = () => {
         <header className={styles.header}>
             <div className={styles.contact}>
                 <div className={styles.bio}>
-                    <div className={styles.avatar}><img src={AVATAR} alt={NAME} /></div>
+                    <div className={styles.avatar}><img src={AVATAR} alt={NAME}/></div>
                     <div className={styles.contactPersonal}>
                         <div className={styles.name}>{NAME}</div>
                         <div className={styles.surname}>{SURNAME}</div>
                         <div className={styles.role}>{ROLE}</div>
                         <div className={styles.social}>
-                            <Social link={SOCIAL_LINK_GIT} img={gitImage} name='git' />
+                            <Social link={SOCIAL_LINK_GIT} img={gitImage} name='git'/>
                             <Social link={SOCIAL_LINK_MAIL} img={mailImage} name='mail'/>
                         </div>
                     </div>
                 </div>
                 <div className={styles.footer}>
-                    {CONTACTS_HEADER.map((item) => <Contacts key={item.id} heading={item.heading} value={item.value} />)}
+                    {CONTACTS_HEADER.map((item) => <Contacts key={item.id} heading={item.heading} value={item.value}
+                                                             link={item.link}/>)}
                 </div>
             </div>
         </header>
